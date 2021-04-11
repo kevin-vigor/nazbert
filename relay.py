c6214bc6904b8cc154fb540e610fd3d1b805545e
Kevin Vigor <kvigor@gmail.com>
2021-04-10 18:08:03 -0600

    relay control

--- relay.py
+++ relay.py
@@ -6,6 +6,6 @@ import time
 bus = smbus.SMBus(1)
 
 # Toggle relay 2 (0x10 is i2c channel)
-bus.write_byte_data(0x10, 2, 0xff)
-time.sleep(1)
-bus.write_byte_data(0x10, 2, 0)
+bus.write_byte_data(0x10, 1, 0xff)
+time.sleep(5)
+bus.write_byte_data(0x10, 1, 0)
