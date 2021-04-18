3f05f33d07d08fd8d06f2a52843e6bba905101dc
Kevin Vigor <kvigor@gmail.com>
2021-04-17 18:45:35 -0600

    Almost there w/ the scanner...

--- src/Scanner.cpp
+++ src/Scanner.cpp
@@ -2,6 +2,8 @@
 #include <bluetooth/hci.h>
 #include <bluetooth/hci_lib.h>
 #include <spdlog/spdlog.h>
+#include <sys/select.h>
+#include <unistd.h>
 
 #include <cstring>
 
@@ -68,17 +70,80 @@ int Scanner::scan() {
 
 int Scanner::checkAdvertisingDevices() {
   struct hci_filter originalFilter, newFilter;
-  socklen_t olen;
+  socklen_t originalFilterLen;
   
-  if (getsockopt(hcidev_, SOL_HCI, HCI_FILTER, &originalFilter, &olen) < 0) {
+  if (getsockopt(hcidev_, SOL_HCI, HCI_FILTER, &originalFilter, &originalFilterLen) < 0) {
     spdlog::warn("Cannot get HCI filter: {}", strerror(errno));
     return -1;
   }
 
   hci_filter_clear(&newFilter);
+	hci_filter_set_ptype(HCI_EVENT_PKT, &newFilter);
+	hci_filter_set_event(EVT_LE_META_EVENT, &newFilter);
+
+  if (setsockopt(hcidev_, SOL_HCI, HCI_FILTER, &newFilter, sizeof(newFilter)) < 0) {
+    spdlog::warn("Cannot set HCI filter: {}", strerror(errno));
+    return -1;
+  }
 
 
-  return 0; 
+  struct timeval timeout;
+  timeout.tv_sec = 5; // FIXME!! configurable!!
+  timeout.tv_usec = 0;
+  fd_set readFds;
+  FD_ZERO(&readFds);
+  FD_SET(hcidev_, &readFds);
+
+  int rc;
+  while ((rc = select(1, &readFds, nullptr, nullptr, &timeout)) > 0) {
+    uint8_t buffer[HCI_MAX_EVENT_SIZE];
+    ssize_t len;
+
+    len = read(hcidev_, buffer, sizeof(buffer));
+
+    if (len < 0) {
+      if (errno == EINTR || errno == EAGAIN) {
+        continue;
+      }
+
+      spdlog::warn("read() from HCI device failed: {}", strerror(errno));
+      // Might as well keep trying I guess?
+      continue;
+    }
+
+    assert(len > HCI_EVENT_HDR_SIZE + sizeof(evt_le_meta_event));
+ 
+    evt_le_meta_event *meta = (evt_le_meta_event*)&buffer[1 + HCI_EVENT_HDR_SIZE];
+
+    if (meta->subevent != EVT_LE_ADVERTISING_REPORT) {
+      continue;
+    }
+ 
+    le_advertising_info *info = (le_advertising_info *) (meta->data + 1);
+
+      char addr[18];
+      char name[30];
+
+      memset(name, 0, sizeof(name));
+
+      ba2str(&info->bdaddr, addr);
+
+      printf("I see %s!!\n", addr);
+    
+  }
+
+  if (rc < 0) {
+    spdlog::warn("select() failed: {}", strerror(errno));
+  }
+
+  
+
+  if (setsockopt(hcidev_, SOL_HCI, HCI_FILTER, &originalFilter, originalFilterLen) < 0) {
+    spdlog::warn("Cannot restore HCI filter: {}", strerror(errno));
+  }
+
+
+  return rc; 
 }
 
 
